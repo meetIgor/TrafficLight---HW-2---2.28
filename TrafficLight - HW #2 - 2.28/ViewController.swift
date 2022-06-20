9b14406d6174395515c8ffdc606b32f1648d38f7
meetIgor <iigoor.s@gmail.com>
2022-06-20 22:24:08 +0300

    last version

--- TrafficLight - HW #2 - 2.28/ViewController.swift
+++ TrafficLight - HW #2 - 2.28/ViewController.swift
@@ -7,13 +7,19 @@
 
 import UIKit
 
+
+
 class ViewController: UIViewController {
+    private enum CurrentLight {
+        case red, yellow, green
+    }
     
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
     @IBOutlet weak var startButton: UIButton!
     
+    private var currentLight = CurrentLight.red
     private let lightON = 1.0
     private let lightOFF = 0.5
     
@@ -21,22 +27,44 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        redLight.alpha = 0.5
-        yellowLight.alpha = 0.5
-        greenLight.alpha = 0.5
-        
-        redLight.layer.cornerRadius = redLight.frame.width / 2
-        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
-        greenLight.layer.cornerRadius = greenLight.frame.width / 2
-        
         startButton.layer.cornerRadius = 15
         
+        redLight.alpha = lightOFF
+        yellowLight.alpha = lightOFF
+        greenLight.alpha = lightOFF
         
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = redLight.frame.width / 2
+        greenLight.layer.cornerRadius = redLight.frame.width / 2
     }
     
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
         
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentLight {
+        case .red:
+            redLight.alpha = lightON
+            greenLight.alpha = lightOFF
+            currentLight = .yellow
+        case .yellow:
+            yellowLight.alpha = lightON
+            redLight.alpha = lightOFF
+            currentLight = .green
+        case .green:
+            greenLight.alpha = lightON
+            yellowLight.alpha = lightOFF
+            currentLight = .red
+        }
+        
+        /*
+         
         switch startButton.tag {
         case 1:
             redLight.alpha = lightON
@@ -55,7 +83,7 @@ class ViewController: UIViewController {
         }
         
         startButton.tag += 1
-        
+         */
     }
     
     
