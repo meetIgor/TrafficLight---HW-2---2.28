9d74483cf989b39f2a747a7826fc91dfcfdedf03
meetIgor <iigoor.s@gmail.com>
2022-06-20 14:43:31 +0300

    changes according to my HW mistakes

--- TrafficLight - HW #2 - 2.28/ViewController.swift
+++ TrafficLight - HW #2 - 2.28/ViewController.swift
@@ -8,22 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     @IBOutlet weak var redLight: UIView!
     @IBOutlet weak var yellowLight: UIView!
     @IBOutlet weak var greenLight: UIView!
     @IBOutlet weak var startButton: UIButton!
     
-    
-    
-    override func viewDidLayoutSubviews() {
-        redLight.layer.cornerRadius = redLight.frame.width / 2
-        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
-        greenLight.layer.cornerRadius = greenLight.frame.width / 2
-        
-        startButton.layer.cornerRadius = 15
-    }
-    
+    private let lightON = 1.0
+    private let lightOFF = 0.5
     
     
     override func viewDidLoad() {
@@ -33,67 +25,52 @@ class ViewController: UIViewController {
         yellowLight.alpha = 0.5
         greenLight.alpha = 0.5
         
-        
-        /*
-        
         redLight.layer.cornerRadius = redLight.frame.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.width / 2
         
         startButton.layer.cornerRadius = 15
-         */
-    
+        
+        
     }
-    
-    var activeLight = ""
     
     
     @IBAction func startButtonTapped(_ sender: UIButton) {
-
-        if startButton.currentTitle == "START" {
-            activeLight = "red"
-        }
         
-        startButton.setTitle("NEXT", for: .normal)
-
-        switch activeLight {
-        case "red":
-            redLight.alpha = 1
-            greenLight.alpha = 0.5
-            activeLight = "yellow"
-        case "yellow":
-            redLight.alpha = 0.5
-            yellowLight.alpha = 1
-            activeLight = "green"
-        case "green":
-            yellowLight.alpha = 0.5
-            greenLight.alpha = 1
-            activeLight = "red"
-        default:
-            activeLight = "red"
-        }
-        
-        
-        
-        /*
         switch startButton.tag {
         case 1:
-            redLight.alpha = 1
-            greenLight.alpha = 0.5
+            redLight.alpha = lightON
+            greenLight.alpha = lightOFF
         case 2:
-            redLight.alpha = 0.5
-            yellowLight.alpha = 1
+            redLight.alpha = lightOFF
+            yellowLight.alpha = lightON
         case 3:
-            yellowLight.alpha = 0.5
-            greenLight.alpha = 1
+            yellowLight.alpha = lightOFF
+            greenLight.alpha = lightON
             startButton.tag = 0
         default:
-            startButton.tag = 0
+            startButton.setTitle("NEXT", for: .normal)
+            redLight.alpha = lightON
+            startButton.tag = 1
         }
-         */
+        
+        startButton.tag += 1
         
     }
     
     
 }
 
+
+
+
+
+/*
+override func viewDidLayoutSubviews() {
+    redLight.layer.cornerRadius = redLight.frame.width / 2
+    yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+    greenLight.layer.cornerRadius = greenLight.frame.width / 2
+    
+    startButton.layer.cornerRadius = 15
+}
+ */
